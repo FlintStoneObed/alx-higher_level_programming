960cd69d644d1d414e3e90f7a7ee14ce1e178c2f
Obed Kwartekwei Quartey <okquartey@st.ug.edu.gh>
2024-01-02 13:58:43 -0800

    2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 
-for letter in range(97, 123):
-    print("{}".format(chr(letter)), end="")
+def print_alphabet() -> None:
+
+    for letter in range(97, 123):
+        print("{:c}".format(letter), end="")
+
+
+if __name__ == "__main__":
+    print_alphabet()
