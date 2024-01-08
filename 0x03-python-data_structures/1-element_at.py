9fb8ea91381b22fe28a40ef460e4d3e0c0d6f247
Obed Kwartekwei Quartey <okquartey@st.ug.edu.gh>
2024-01-08 05:30:28 -0800

    Python - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
-def element_at(list, idx):
-    if idx > (len(list) - 1):
-        return None
-    idx = list[idx]
-    if idx < 0:
-        return None
-    else:
-        return idx
+def element_at(my_list, idx):
+        return(my_list[idx] if 0 <= idx < len(my_list) else "None")
