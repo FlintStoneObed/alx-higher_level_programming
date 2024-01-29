c85e13defc2a98ac23ff6ec89e40b0bb0a470b42
Obed Kwartekwei Quartey <okquartey@st.ug.edu.gh>
2024-01-29 05:14:43 -0800

    updated

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -2,27 +2,32 @@
 
 /**
  * print_python_string - Prints information about Python strings.
- * @p: A PyObject string object.
+ * @p: The Python object to work with. (Must be a string object).
  */
 void print_python_string(PyObject *p)
 {
-	long int length;
-
-	fflush(stdout);
+	ssize_t size;
 
 	printf("[.] string object info\n");
-	if (strcmp(p->ob_type->tp_name, "str") != 0)
+
+	/* ensure the object is a valid string object */
+	if (strcmp((p->ob_type)->tp_name, "str") != 0)
 	{
 		printf("  [ERROR] Invalid String Object\n");
 		return;
 	}
 
-	length = ((PyASCIIObject *)(p))->length;
-
 	if (PyUnicode_IS_COMPACT_ASCII(p))
+	{
 		printf("  type: compact ascii\n");
+	}
 	else
+	{
 		printf("  type: compact unicode object\n");
-	printf("  length: %ld\n", length);
-	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &length));
+	}
+
+	/* get the size/length of the  string object */
+	size = ((PyASCIIObject *)p)->length;
+	printf("  length: %ld\n", size);
+	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &size));
 }
