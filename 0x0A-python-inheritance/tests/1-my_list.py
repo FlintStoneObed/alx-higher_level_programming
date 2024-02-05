a66a0a1bf9b8876ed19cb703b415a84e7f0709e7
Obed Kwartekwei Quartey <okquartey@st.ug.edu.gh>
2024-02-05 08:26:54 -0800

    test cases

---
+++ 0x0A-python-inheritance/tests/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+defining a child class
+"""
+
+class MyList(list):
+    """
+    defined a child class that inherits the parent class list
+    """
+
+    def print_sorted(self):
+        print(sorted(self))
