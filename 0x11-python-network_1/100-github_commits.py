cc018275855494a81408592bda9da1fef41af375
FlintStoneObed <141341733+FlintStoneObed@users.noreply.github.com>
2024-04-26 14:38:41 +0000

    Update 100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
-"""Take GitHub credentials (username and password) as arguments
-and uses the GitHub API to display users id"""
+"""Listing 10 commits in a repo"""
 
 if __name__ == '__main__':
     import sys
     import requests
 
-    url = "https://api.github.com/user"
-    response = requests.get(
-        url, auth=requests.auth.HTTPBasicAuth(sys.argv[1], sys.argv[2]))
-    res_dict = response.json()
-    print(res_dict.get('id'))
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(
+        sys.argv[2], sys.argv[1])
+    try:
+        response = requests.get(url)
+        res_dict = response.json()
+        for i in range(0, 10):
+            print("{}: {}".format(res_dict[i].get('sha'), res_dict[i].get(
+                'commit').get('author').get('name')))
+    except Exception:
+        pass
